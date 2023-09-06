4bf40f85806bacfdfed743c3bdfe221d32e619d0
ankit-cerulean-1 <palliankit123@gmail.com>
2023-09-06 15:42:43 +0530

    Added Timer

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = require('express')();
  * Problem Statement: Given list of items in array find maximum repeated item and display it on output.
  * Input: List of Elements
  * Output: Most Repeated String in Array
+ * To Run Program type : 'npx nodemon index.js' in terminal and wait
+ * To inspect and check console logs right click and inspect in chrome.
  */
 var getMaxRepeatedString = (req, res) => {
   let input = [
@@ -15,7 +17,7 @@ var getMaxRepeatedString = (req, res) => {
       'computer',
       'table',
     ],
-    output = 'Display String with Maximum repeated count in Array';
+    output = '?';
 
   /**
    * Write down your code here
@@ -23,7 +25,58 @@ var getMaxRepeatedString = (req, res) => {
 
   /** Logic Ends */
 
-  res.send('<h3>' + output + '</h3>');
+  res.send(
+    ` <h1 style="color:red">Timer: <span id="displayDiv"></span>
+    <br><button onclick="resetTimer()">Reset</button><hr>
+    <h4> Problem Statement: Given list of items in array find maximum repeated item and display it on output.</h4>
+    <h1> Output: ` +
+      output +
+      '</h1>' +
+      `<script>
+      const COUNTER_KEY = 'my-counter';
+      var minutes=0, seconds = 0, timerSecs = 300,cntr = 0;
+
+      function countDown(i, callback) {
+        //callback = callback || function(){};
+          timer = setInterval(function () {
+        
+          minutes = parseInt(i / 60, 10);
+          seconds = parseInt(i % 60, 10);
+          minutes = minutes < 10 ? '0' + minutes : minutes;
+          seconds = seconds < 10 ? '0' + seconds : seconds;
+
+          if(window.sessionStorage.getItem(COUNTER_KEY)){
+            i = window.sessionStorage.getItem(COUNTER_KEY);
+            minutes = parseInt(i / 60, 10);
+            seconds = parseInt(i % 60, 10);
+          }
+          if (i-- > 0) {
+            window.sessionStorage.setItem(COUNTER_KEY, i);
+          } else {
+            clearInterval(timer);
+            callback();
+          }
+
+          window.document.getElementById('displayDiv').innerHTML =
+          '0:' +
+          minutes +
+          ':' +
+          seconds;
+
+        }, 1000);
+      }
+      function resetTimer(){
+        cntr++;
+        if(cntr == 3){
+          clearInterval(timer);
+          window.sessionStorage.setItem(COUNTER_KEY, timerSecs);
+          countDown(timerSecs, () => {});
+          cntr=0;
+        }
+      }
+      countDown(timerSecs, () => {});
+      </script>`
+  );
 };
 
 app.get('/', getMaxRepeatedString);
